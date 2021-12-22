5048904da2adbf89a4545397c222df261257fed9
TheGirlWhoEncrypts <thegirlwhoencrypts@gmail.com>
2021-12-22 20:13:33 +0800

    Done

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -4,4 +4,4 @@ from src.hello_world import greet
 
 class TestHelloWorld(unittest.TestCase):
     def test_greet_returns_hello_world(self):
-        self.assertEquals("Hello world!", greet())
+        self.assertEqual("Hello world!", greet())
