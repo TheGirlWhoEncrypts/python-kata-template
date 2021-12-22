4d39535e20fe33fc823c309b810357cdaa26c151
TheGirlWhoEncrypts <thegirlwhoencrypts@gmail.com>
2021-12-22 20:43:32 +0800

    Added wardobe_1

---
+++ src/wardrobe_1.py
@@ -0,0 +1,15 @@
+def wardrobe(sizes, total):
+    results = []
+
+    if total == 0:
+        return results
+
+    def recursion(subset=[]):
+        for s in sizes:
+            if sum(subset + [s]) < total:
+                recursion(subset + [s])
+            elif sum(subset + [s]) == total:
+                results.append(sorted(subset + [s]))
+
+    recursion()
+    return list(map(list, sorted(set(map(tuple, results)))))
---
+++ tests/test_wardrobe_1.py
@@ -0,0 +1,22 @@
+import unittest
+from src.wardrobe_1 import wardrobe
+
+
+class TestWardrobe(unittest.TestCase):
+    # Remarks: Good start
+    def test_wardrobe_size_0_return_an_empty(self):
+        sizes, total = [1, 2, 3], 0
+        self.assertListEqual([], wardrobe(sizes, total))
+
+    # Remarks: Bad -> (1) Jump; (2) Only 1 assert statement
+    def test_wardrobe_return_all_combinations(self):
+        sizes, total = [2, 4, 6, 8], 8
+        self.assertListEqual(
+            [[2, 2, 2, 2], [2, 2, 4], [2, 6], [4, 4], [8]],
+            wardrobe(sizes, total))
+
+        sizes, total = [50, 75, 100, 129], 250
+        self.assertListEqual(
+            [[50, 50, 50, 50, 50], [50, 50, 50, 100], [
+                50, 50, 75, 75], [50, 100, 100], [75, 75, 100]],
+            wardrobe(sizes, total))
